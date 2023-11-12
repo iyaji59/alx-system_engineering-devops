44bd28f79a245806dd18e3678338c35f88660bf5
tonybruce <iyajianthony59@gmail.com>
2023-11-12 23:15:33 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-# get subs
-from requests import get
-from sys import argv
+'''A module containing functions for working with the Reddit API.
+'''
+import requests
+
+
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
+
 
 def number_of_subscribers(subreddit):
-    """ get number of subscribers """
-    head = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    count = get('https://www.reddit.com/r/{}/about.json'.format(
-        subreddit),headers=head).json()
-    try:
-        return count['data']['subscribers']
-    except:
-        return 0
-
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
+    '''Retrieves the number of subscribers in a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    res = requests.get(
+        '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
+    return 0
